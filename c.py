d98a6f9cbf4bdd1fcb5624bd8f4358dcc86052f0
ncornman <ncornman@stanford.edu>
2016-01-29 17:08:13 -0800

    c.py

---
+++ c.py
@@ -0,0 +1,14 @@
+import os
+import json
+fname = os.path.join('tempdata', 'googlemaps', 'stanford.json')
+
+ffile = open(fname, 'r')
+txt = ffile.read()
+ffile.close()
+
+address = json.loads(txt)
+
+for x in address['results']:
+	print(x['formatted_address'])
+
+ffile.close()
